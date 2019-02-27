ea16a62cf3b69c899e02d206d0f43660f042f29b
ChauDinh <chaudvb95uc@gmail.com>
2019-02-27 11:33:12 +0700

    Split the main function into saparate functions holding one function

--- index.js
+++ index.js
@@ -2,35 +2,43 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const imageDownloader = require("image-downloader");
 
-async function main() {
-  // Open new page, then go to that page
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/phammingngoc/");
-
-  // If there is no folder called "result", then create
-  if (!fs.existsSync("./result")) {
-    fs.mkdirSync("./result");
-  }
-  // Get image attribute, called "srcset"
-  const imageSrcSets = await page.evaluate(() => {
-    const imgs = Array.from(document.querySelectorAll("article img"));
-    const srcSetAttribute = imgs.map(i => i.getAttribute("srcset"));
-    return srcSetAttribute;
-  });
-
-  await browser.close();
-
-  // Process and download images
-  for (let i = 0; i < imageSrcSets.length; i++) {
-    const srcSet = imageSrcSets[i];
-    const splitedSrc = srcSet.split(",");
-    const imgSrc = splitedSrc[splitedSrc.length - 1].split(" ")[0];
-    
-    imageDownloader({
-      url: imgSrc,
-      dest: "./result"
-    });
-  }
+function getLargestImageFromSrcSet(srcSet) {
+  const splitedSrcs = srcSet.split(",");
+  const imgSrc = splitedSrcs[splitedSrcs.length - 1].split(" ")[0];
+  return imgSrc;
 }
+
+async function getImagesFromPage(url) {
+ // Use puppeteer, can not use normal html parser because...
+ const browser = await puppeteer.launch();
+ const page = await browser.newPage();
+ await page.goto(url);
+
+ const imageSrcSets = await page.evaluate(() => {
+  // This might be changed if Instagram change their layout
+  const imgs = Array.from(document.querySelectorAll("article img"));
+  const srcSetAttribute = imgs.map(i => i.getAttribute("srcset"));
+  return srcSetAttribute;
+ });
+
+ const imgUrls = imageSrcSets.map(imageSrcSet => getLargestImageFromSrcSet(imageSrcSet));
+ await browser.close();
+ return imgUrls;
+}
+
+async function main() {
+ const resultFolder = "./result";
+  if (!fs.existsSync(resultFolder)) {
+    fs.mkdirSync(resultFolder);
+  }
+  let inputUrl = 'https://www.instagram.com/phammingngoc/';
+  const images = await getImagesFromPage(inputUrl);
+  images.forEach(image => {
+   imageDownloader({
+    url: image,
+    dest: resultFolder
+   })
+  })
+}
+
 main();
